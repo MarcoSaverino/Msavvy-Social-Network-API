3975e0aab7e758e3c57f9afcf4e59801a6db665e
MarcoSaverino <marcosaverino@live.ca>
2023-07-16 20:44:09 -0400

    added some code to start and installed dependencies

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+mongoose.set("strictQuery", true);
+
+module.exports = mongoose.connection;
